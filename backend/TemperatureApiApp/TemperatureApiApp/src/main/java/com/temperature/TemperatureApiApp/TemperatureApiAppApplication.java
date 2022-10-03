bb88c61693923528f10a83222007821f1a3a3a77
fs2000817-gotou-kei <k_goto@freestyles.jp>
2022-10-04 08:11:40 +0900

    first commit

---
+++ backend/TemperatureApiApp/TemperatureApiApp/src/main/java/com/temperature/TemperatureApiApp/TemperatureApiAppApplication.java
@@ -0,0 +1,12 @@
+package com.temperature.TemperatureApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureApiAppApplication.class, args);
+	}
+}
---
+++ backend/TemperatureApiApp/TemperatureApiApp/src/test/java/com/temperature/TemperatureApiApp/TemperatureApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temperature.TemperatureApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
